3573c6e9ff1786380f5bf85be603ce10c50355ff
Edmond Lee <techhexium@gmail.com>
2018-12-05 17:14:33 -0800

    WIP on pagination, API call limits to 10 items

--- challenge_1/client/components/app.jsx
+++ challenge_1/client/components/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactPaginate from 'react-paginate';
 
-const YEAR = 0, DESCRIPTION = 1;
+const YEAR = 0, DESCRIPTION = 1, ITEMS_PER_PAGE = 10;
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,22 +10,42 @@ export default class App extends React.Component {
     this.state = {
       mode: YEAR,
       events: [],
+      offset: 0 // Number of items to offset, divide by items per page to get page number
     }
 
     this.onTypeChange = this.onTypeChange.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
     this.onSearchClicked = this.onSearchClicked.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
     this.query = '';
   }
 
-  static getDataByDate(date) {
-    return fetch(`/events?date=${date}`)
+  static getDataByDate(date, offset, limit) {
+    return fetch(`/events?date=${date}&_page=${Math.floor(offset / limit)}&_limit=${limit}`)
       .then(res => res.json());
   }
 
-  static getDataByText(text) {
-    return fetch(`/events?q=${text}`)
+  static getDataByText(text, offset, limit) {
+    return fetch(`/events?q=${text}&_page=${Math.floor(offset / limit)}&_limit=${limit}`)
       .then(res => res.json());
+  }
+
+  // Page clicked, load relevant data
+  handlePageClick(data) {
+    let selected = data.selected;
+    let offset = Math.ceil(selected * ITEMS_PER_PAGE);
+
+    this.setState({offset}, () => {
+      let promise = mode === DESCRIPTION ?
+        App.getDataByText(this.query, offset) : App.getDataByDate(this.query, offset, ITEMS_PER_PAGE);
+
+      promise
+        .then(events => {
+          this.setState({
+            events
+          });
+        });
+    });
   }
 
   onTypeChange(mode) {
@@ -33,43 +54,64 @@ export default class App extends React.Component {
 
   onInputChange(event) {
     this.query = event.target.value;
+    if(!this.query) {
+      this.setState({ events: [], pageCount: 0 });
+    }
   }
 
   onSearchClicked() {
-    const { mode } = this.state;
-    let promise = mode === DESCRIPTION ?
-      App.getDataByText(this.query) : App.getDataByDate(this.query);
+    const { mode, offset } = this.state;
 
-    promise
-      .then(events => {
-        this.setState({
-          events
+    if (this.query) {
+      let promise = mode === DESCRIPTION ?
+        App.getDataByText(this.query, offset, ITEMS_PER_PAGE) : App.getDataByDate(this.query, offset, ITEMS_PER_PAGE);
+
+      promise
+        .then(events => {
+          this.setState({
+            events
+          });
         });
-      });
+    }
   }
 
   render() {
-    const { mode, events } = this.state;
+    const { mode, events, offset } = this.state;
 
     return (
       <div>
         <div id="searchUI">
-          <button type="button" className="searchEle" onClick={this.onTypeChange.bind(this, YEAR)}>Year</button>
-          <button type="button" className="searchEle" onClick={this.onTypeChange.bind(this, DESCRIPTION)}>Description</button>
+          <button type="button" className="searchEle" onClick={this.onTypeChange.bind(this, YEAR)}>Search By Year</button>
+          <button type="button" className="searchEle" onClick={this.onTypeChange.bind(this, DESCRIPTION)}>Search By Description</button>
           <input type="text" className="searchEle" placeholder={ mode ? 'Search by text...' : 'Search by year...' } onChange={this.onInputChange} />
           <button type="button" onClick={this.onSearchClicked}>Search</button>
         </div>
         <div id="eventList">
           { events.length > 0
             ? (
-              <ul>
-                {events.map(obj => (
-                  <li className="eventItem">
-                    <p>{obj.date}</p>
-                    <p>{obj.description}</p>
-                  </li>
-                ))}
-              </ul>
+              <div>
+                <ul>
+                  {events.map(obj => (
+                    <li className="eventItem">
+                      <p>{obj.date}</p>
+                      <p>{obj.description}</p>
+                    </li>
+                  ))}
+                </ul>
+                <div id="react-paginate">
+                  <ReactPaginate previousLabel={"previous"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={Math.floor(offset / ITEMS_PER_PAGE)}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"} />
+                 </div>
+              </div>
             ): <h3>Historical events will show up here!</h3>
           }
         </div>
