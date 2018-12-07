15812db70409f4624c6a9e4190b8faf7d6e450c9
Edmond Lee <techhexium@gmail.com>
2018-12-07 11:23:29 -0800

    Allow user to enter their name

--- challenge_3/client/components/app.jsx
+++ challenge_3/client/components/app.jsx
@@ -9,10 +9,25 @@ export default class App extends React.Component {
       
     };
   }
-  
+
   render() {
+    const { name } = this.state;
+
+    if(!name) {
+      return (
+        <div>
+          <label htmlFor="name">
+            Enter your name:
+            <input type="text" id="name" onChange={ (event) => { this.name = event.target.value; } } />
+          </label>
+          <button type="button" onClick={ () => { this.setState({ name: this.name }); } }>Play!</button>
+        </div>
+      );
+    }
+
     return (
       <div>
+        
       </div>
     );
   }
