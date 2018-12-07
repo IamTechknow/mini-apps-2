390fb43c4ac4abf61424d82e3a69af8319abe598
Edmond Lee <techhexium@gmail.com>
2018-12-06 16:25:50 -0800

    Set up development environment

---
+++ challenge_2/client/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chart from 'chart.js';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
